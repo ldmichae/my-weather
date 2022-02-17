efd813ca6ea8eeca11866aeaa1e70f48948496fb
ldmichae <l96mich@gmail.com>
2022-02-16 20:55:29 -0500

    `

--- weather.js
+++ weather.js
@@ -3,5 +3,5 @@ const weather = require('weather-js');
 weather.find({search: '27513', degreeType: 'F'}, function(err, result) {
   if(err) console.log(err);
  
-  console.log(JSON.stringify(result, null, 2));
+  return JSON.stringify(result, null, 2);
 });
